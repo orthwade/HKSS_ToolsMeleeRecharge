ed08138ac83863c1838298b949b02b6a5879eb7b
orthwade <orthwade@protonmail.com>
2025-09-17 21:14:51 +0500

    fix logger

--- ToolsMeleeRecharge/ToolsMeleeRecharge.cs
+++ ToolsMeleeRecharge/ToolsMeleeRecharge.cs
@@ -1,15 +1,12 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 using HarmonyLib;
 using owd;
-using ToolsMeleeRecharge.Events;
 
 namespace ToolsMeleeRecharge
 {
     [BepInPlugin("com.orthwade.toolsmeleerecharge", "Tools Melee Recharge", "1.0.6")]
     public class ToolsMeleeRecharge : BaseUnityPlugin
     {
-        private static ManualLogSource logger;
         internal static ToolsMeleeRecharge Instance;
         private void Awake()
         {
@@ -17,12 +14,11 @@ namespace ToolsMeleeRecharge
 
             BepinexPluginLogger.Init(Config, "ToolsMeleeRecharge");
 
-            logger = BepinexPluginLogger.GetLogger();
             GlobalToolConfig.Init(Config);
 
             ToolLibrary.Init(Config);
 
-            logger.LogInfo("Tools Melee Recharge loaded!");
+            BepinexPluginLogger.LogInfo("Tools Melee Recharge loaded!");
 
             // Apply Harmony patches
             var harmony = new Harmony("com.orthwade.toolsmeleerecharge");
